bd4fbcc7a764ed5f4957ffb6d1c72dfec7aac175
BLooDBRothER <computertechnology446@gmail.com>
2022-08-17 17:33:04 +0530

    Create message view and its link

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+const messages = [
+  {
+    title: 'sample 1',
+    text: "Hi there!",
+    user: "Amando",
+    added: (new Date()).toLocaleDateString()
+  },
+  {
+    title: 'sample 2',
+    text: "Hello World!",
+    user: "Charles",
+    added: (new Date()).toLocaleDateString()
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages });
 });
 
 module.exports = router;
