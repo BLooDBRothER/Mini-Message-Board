0e7c0266edb6f83b6165790e3106a89879c27f37
BLooDBRothER <computertechnology446@gmail.com>
2022-08-18 08:54:48 +0530

    Update UI with moment.js

--- message_data.js
+++ message_data.js
@@ -1,15 +1,23 @@
+const moment = require('moment');
+
 const messages = [
     {
       title: 'sample 1',
       text: "Hi there!",
       user: "Amando",
-      added: (new Date()).toLocaleDateString()
+      added: moment("18/08/2022", "DD/MM/YYYY").fromNow()
     },
     {
       title: 'sample 2',
       text: "Hello World!",
       user: "Charles",
-      added: (new Date()).toLocaleDateString()
+      added: moment("11/04/2022", "DD/MM/YYYY").fromNow()
+    },
+    {
+      title: 'sample 3',
+      text: "Fun Fun Fun!",
+      user: "Donald",
+      added: moment("2022-08-18T03:10:37.740Z").fromNow()
     }
 ];
 
--- routes/new.js
+++ routes/new.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const moment = require('moment');
+
 const messages = require('../message_data');
+
+const addedDateString = () => {
+  return moment().toDate();
+}
 
 /* Create New Message. */
 router.get('/', function(req, res, next) {
@@ -10,7 +16,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/', (req, res, err) => {
   const data = req.body;
-  const message = {...data,  added: (new Date()).toLocaleDateString()};
+  const message = {...data,  added: moment(addedDateString()).fromNow()};
   messages.push(message);
   return res.redirect('/');
 });
